7d236ed497ca976fdafe140e9c81aec051914711
Jesus Alastuey <jaalastuey@gmail.com>
2022-07-15 11:19:52 +0200

    17

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Home() {
+  const [estado, actualizarEstado] = useState('Jesus')
+  console.log({estado}) 
   return (
     <div>
-      <h1>Hola Mundo!</h1>
-      <Link href= '/products/create'>Crear Producto</Link>
+      <h1>Hola Mundos!</h1>
+      <Link href= '/products/create'>Crear Producto</Link><br></br>
+      <Link href="/contactos/listado">Listado Contacto</Link>
     </div>
   )
 }
